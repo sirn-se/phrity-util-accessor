8a0d60abb79bbac13233e9dde089381a4850bbbb
Sören Jensen <sirn@sirn.se>
2025-05-02 09:50:00 +0200

    Coercion using Transformers

--- src/Transformer/TransformerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Phrity\Util\Transformer;
-
-interface TransformerInterface
-{
-    public function canTransform(mixed $subject, string|null $type = null): bool;
-    public function transform(mixed $subject, string|null $type = null): mixed;
-}
--- tests/AccessorTest.php
+++ tests/AccessorTest.php
@@ -8,6 +8,8 @@ use PHPUnit\Framework\TestCase;
 use Phrity\Util\Accessor;
 use Phrity\Util\Transformer\{
     BasicTypeConverter,
+    FirstMatchResolver,
+    ReadableConverter,
     Type,
 };
 
@@ -191,5 +193,69 @@ class AccessorTest extends TestCase
         $clone = $accessor->set($subject, 'public', 'Changed public');
         $this->assertNotSame($subject, $clone);
         $this->assertEquals('Changed public', $clone->public);
+    }
+
+    public function testTransformers(): void
+    {
+        $transformer = new FirstMatchResolver([
+            new ReadableConverter(),
+            new BasicTypeConverter(),
+        ]);
+        $accessor = new Accessor('.', $transformer);
+        $subject = [
+            'string-val' => 'A string',
+            'null-val' => null,
+            'assoc-array-val' => [
+                'string-val-2' => 'Another string',
+                'null-val-2' => null,
+            ],
+            'num-array-val' => [
+                'a',
+                'b',
+                'c',
+            ],
+            'object-val' => (object)[
+                'string-val-3' => 'Yet another string',
+                'null-val-3' => null,
+            ],
+        ];
+        $result = $accessor->get($subject, 'string-val');
+        $this->assertEquals('A string', $result);
+        $result = $accessor->get($subject, 'null-val', coerce: Type::STRING);
+        $this->assertEquals('null', $result);
+        $result = $accessor->get($subject, 'non-existing', 'The default');
+        $this->assertEquals('The default', $result);
+        $result = $accessor->get($subject, '', 'The default');
+        $this->assertEquals($subject, $result);
+        $result = $accessor->get($subject, 'assoc-array-val', 'The default');
+        $this->assertEquals([
+            'string-val-2' => 'Another string',
+            'null-val-2' => null,
+        ], $result);
+        $result = $accessor->get($subject, 'assoc-array-val.string-val-2', 'The default');
+        $this->assertEquals('Another string', $result);
+        $result = $accessor->get($subject, 'assoc-array-val.null-val-2', 'The default');
+        $this->assertNull($result);
+        $result = $accessor->get($subject, 'num-array-val', 'The default');
+        $this->assertEquals(['a', 'b', 'c'], $result);
+        $result = $accessor->get($subject, 'num-array-val.0', 'The default');
+        $this->assertEquals('a', $result);
+        $result = $accessor->get($subject, 'num-array-val.1', 'The default');
+        $this->assertEquals('b', $result);
+        $result = $accessor->get($subject, 'num-array-val.2', 'The default');
+        $this->assertEquals('c', $result);
+        $result = $accessor->get($subject, 'object-val', 'The default');
+        $this->assertEquals((object)[
+            'string-val-3' => 'Yet another string',
+            'null-val-3' => null,
+        ], $result);
+        $result = $accessor->get($subject, 'object-val.string-val-3', 'The default');
+        $this->assertEquals('Yet another string', $result);
+        $result = $accessor->get($subject, 'object-val.non-existing', 'The default');
+        $this->assertEquals('The default', $result);
+        $result = $accessor->get($subject, 'object-val.null-val-3', coerce: Type::STRING);
+        $this->assertEquals('null', $result);
+        $result = $accessor->get(null, 'non-existing', 'The default');
+        $this->assertEquals('The default', $result);
     }
 }
